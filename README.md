dfc5bd2186ae5327627a21a5de52d963fc830e5a
unknown <ll407290@S202P09.iutnice.unice.fr>
2015-01-14 09:36:58 +0100

    Ajout, TP

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Pictionnary Asynchrone est une application similaire à l'application Draw Something [LPSIL][IDSE]  Cette application est développée en cours lors des séances prévues avec M. Khalil Bouzidi
 
 Créer un dossier 'data' ou autre pour enregistrer les images temporaires
+
+
+Le dossier TP contient tous les tests réussi lors du TP facebook, création Mashup
---
+++ TP/sociaux.php
@@ -0,0 +1,80 @@
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+<div id="fb-root"></div>
+<script>
+	window.fbAsyncInit = function() {
+		FB.init({
+			appId: '735030746591821',
+			status: true,
+			cookie: true,
+			xfbml: true
+		});
+	};
+
+	// Démarre le SDK asynchrone
+	(function(d){
+		var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+		if (d.getElementById(id)) {return;}
+		js = d.createElement('script'); js.id = id; js.async = true;
+		js.src = "//connect.facebook.net/fr_FR/all.js";
+		ref.parentNode.insertBefore(js, ref);
+	}(document));
+
+	function login() {
+		FB.login(function(response) {
+
+		console.log(response);
+
+		}, {scope: 'read_stream,publish_stream,publish_actions,read_friendlists'});            
+	}
+
+	// Déconnecte l'utilisateur
+	function logout() {
+		FB.logout(function(response) {
+			console.log("Utilisateur deconnecter");
+		});
+	}
+
+	// Post un message sur le mur
+	function ui() {
+		FB.ui( {
+				method: 'feed',
+				name: 'The Facebook SDK for Javascript',
+				caption: 'Bringing Facebook to the desktop and mobile web',
+				description: (
+					'A small JavaScript library that allows you to harness ' +
+					'the power of Facebook, bringing the user\'s identity, ' +
+					'social graph and distribution power to your site.'
+				),
+				link: 'https://developers.facebook.com/docs/reference/javascript/',
+				picture: 'http://www.fbrell.com/public/f8.jpg'
+			}, function(response) {
+				if (response && response.post_id) {
+					alert('Post publier !');
+				} else {
+					alert('Post non publier !');
+				}
+		});
+	}
+
+	// Envois un message sur facebook
+	function envoisMessage() {
+		var msg = document.getElementById('msg').value;
+		FB.api('/me/feed', 'post', {message: msg});
+		alert('Le message à été poster');
+	}
+
+	var status = FB.getLoginStatus();
+
+	console.log(status);
+</script>
+<div id="connecter" class="fb-login-button" data-max-rows="1" data-show-faces="true"></div><br>
+<button id="deconnexion" onclick="logout()">Deconnexion</button>
+<button id="deconnexion" onclick="ui()">UI</button>
+<input type="text" id="msg">
+<input type="submit" id="deconnexion" onclick="envoisMessage()" value="Envois">
+</body>
+</html>
